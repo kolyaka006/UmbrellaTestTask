57e4e6582b4328a8693f98065bc05e16bbe12504
Nick Zhigulin <jigasan70@gmail.com>
2017-06-01 15:10:55 +0300

    fix issues on registration page

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,13 +1,17 @@
 //LOGIN MODULE
 function login() {
+    // getting value from input
     var email = $('.login-email').val();
     var password = $('.login-password').val();
+    // checking email not empty
     if (!email) {
         $('.login-email').parent().addClass('error');
     }
+    // checking password not empty
     if (!password) {
         $('.login-password').parent().addClass('error')
     }
+    // checking email and password not empty and get user token
     if (email && password) {
         $.get('/login/sign-in?email=' + email + '&password=' + password).then(function (resp) {
             localStorage.setItem('token', resp.token)
@@ -22,29 +26,40 @@ function login() {
 
 //REGISTRATION MODULE
 function registration() {
+    // getting value from input
     var email = $('.email').val();
     var password = $('.password').val();
     var confirm = $('.confirm').val();
+    // checking email not empty
     if (!email) {
         $('.email').parent().addClass('error');
-        return
     }
+    // checking password not empty
+    if (!password) {
+        $('.password').parent().addClass('error');
+    }
+    // checking a confirmation password
     if (password != confirm) {
         $('.confirm').parent().addClass('error')
         $('.error-confirm').addClass('fade');
         return
     }
-
-    $.post('/registration/create-user', {email: email, password: password}).then(function (resp) {
-        window.location.replace('/login')
-    })
-
+    // checking email and password not empty and sending
+    if (email && password) {
+        $.post('/registration/create-user', {email: email, password: password}).then(function (resp) {
+            window.location.replace('/login')
+        })
+    }
 }
 
 
 $(document).ready(function(){
+    // registration page. Hide error
     $('.email').on('focus', function () {
         $('.email').parent().removeClass('error');
+    })
+    $('.password').on('focus', function () {
+        $('.password').parent().removeClass('error');
     })
 
     $('.confirm').on('focus', function () {
@@ -52,6 +67,7 @@ $(document).ready(function(){
         $('.error-confirm').removeClass('fade');
     })
 
+    // login page. Hide error
     $('.login-email').on('focus', function () {
         $('.login-email').parent().removeClass('error');
         $('.error-login').removeClass('fade');
